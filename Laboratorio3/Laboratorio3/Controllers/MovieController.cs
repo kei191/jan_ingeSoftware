b12a5db1dafbf6130616f7d5399eabb96a209c71
Jan Murillo <janmb191@gmail.com>
2022-04-29 23:20:58 -0600

    FIn de movie lab 3

---
+++ Laboratorio3/Laboratorio3/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio3.Controllers
+{
+    public class MovieController : Controller
+    {
+ 
+        public IActionResult Index()
+        {
+            var movies = GetListOfMovies();
+            ViewBag.MainTitle = " List of my favorite films";
+            return View(movies);
+        }
+        private List <MovieModel> GetListOfMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(new MovieModel { Id = 1, Name = " Pulp Fiction ", Genre = " Crime / Drama ",
+                ReleasedDate = new DateTime(1994, 10, 14) });
+            movies.Add(new MovieModel { Id = 2, Name = " Toy Story ", Genre = " Family / Comedy ",
+                ReleasedDate = new DateTime(1995, 11, 22) });
+            movies.Add(new MovieModel { Id = 3, Name = " Mulan ", Genre = " Family / Comedy ",
+                ReleasedDate = new DateTime(1998, 06, 19) });
+            return movies;
+        }
+    }
+}
