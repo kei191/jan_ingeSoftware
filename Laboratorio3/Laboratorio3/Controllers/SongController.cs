9c32249889e037e8473f041a81c9e489ed37e9a9
Jan Murillo <janmb191@gmail.com>
2022-05-01 11:18:15 -0600

    Parte 2 del laboratorio, añadiendo songs

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+                var songs = GetListOfSongs();
+                ViewBag.MainTitle = " List of my favorite songs";
+                return View(songs);
+
+        }
+        private List<SongModel> GetListOfSongs()
+        {
+            List<SongModel> songs = new List<SongModel>();
+            songs.Add(new SongModel
+            {
+                Id = 1,
+                Title = " 결국 ",
+                Genre = " Pop ",
+                Singer = "G-Dragon",
+                Language = "Korean/English",
+                ReleasedDate = new DateTime(2015, 5, 30)
+            });
+            songs.Add(new SongModel
+            {
+                Id = 2,
+                Title = " ジグソーパズル ",
+                Genre = " Pop/Electronic ",
+                Singer = "鏡音レン",
+                Language = "Japanese",
+                ReleasedDate = new DateTime(2018, 11, 24)
+            });
+            songs.Add(new SongModel
+            {
+                Id = 3,
+                Title = " Nakakapagpabagabag ",
+                Genre = " Pop/Electronic ",
+                Singer = "鏡音レン",
+                Language = "Tagalog",
+                ReleasedDate = new DateTime(2016, 5, 27)
+            });
+            return songs;
+        }
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Singer { get; set; }
+        public string Language { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
