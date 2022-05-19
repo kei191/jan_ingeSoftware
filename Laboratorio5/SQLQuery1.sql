fa126f6a19dea8ba2156c813df15c4ae47f33db8
Jan Murillo <janmb191@gmail.com>
2022-05-18 20:47:22 -0600

    Se arreglaron algunos problemas con el tipo de proyecto

--- Laboratorio5/SQLQuery1.sql
+++
@@ -1,11 +0,0 @@
-﻿CREATE TABLE Pais(
-Id INTEGER PRIMARY KEY IDENTITY,
-Nombre VARCHAR(200) NOT NULL,
-Idioma VARCHAR(200) NOT NULL,
-Continente NVARCHAR(100) NOT NULL
-)INSERT INTO [dbo].[Pais] ([Nombre],[Idioma] ,[Continente])
-VALUES('Costa Rica', 'Español', 'América'),
-('Argentina', 'Español', 'América'),
-('Canada', 'Inglés/Frances', 'América'),
-('Francia', 'Frances', 'Europa'),
-('España', 'Español', 'Europa')Select *From Pais
