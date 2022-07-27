655349c3e9bd265b92bcea048fd4677de7163708
Jan Murillo <janmb191@gmail.com>
2022-07-27 17:52:32 -0600

    Se crearon las clases del backend para el control de los refrescos y las pseudo bases de datos correspondientes

---
+++ ex2_B95447/ex2_backend/Controllers/RefrescosController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ex2_backend.Controllers
+{
+  public class RefrescosController : ControllerBase
+  {
+
+  }
+}
---
+++ ex2_B95447/ex2_backend/Handlers/RefrescosHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ex2_backend.Handlers
+{
+  public class RefrescosHandler
+  {
+    static List<string> nombre = new List<string>()
+    {
+      "Coca-Cola",
+      "Pepsi",
+      "Fanta",
+      "Sprite"
+    };
+
+    static List<int> cantidad = new List<int>()
+    {
+      10,
+      8,
+      10,
+      15
+    };
+
+  }
+}
