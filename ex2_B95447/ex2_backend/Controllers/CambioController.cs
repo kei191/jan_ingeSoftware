63381a09a520e7dadd29f458b9b218ef0f2f5bc5
Jan Murillo <janmb191@gmail.com>
2022-07-27 17:06:27 -0600

    Creación de las clases del backend relacionadas a la obtención del cambio e inicio de la pseudo base de datos.

---
+++ ex2_B95447/ex2_backend/Controllers/CambioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ex2_backend.Controllers
+{
+  [Route("api/")]
+  [ApiController]
+  public class CambioController : ControllerBase
+  {
+    // ejemplo del PI
+    /*
+    [HttpGet]
+    [Route("benefits")]
+    public ActionResult GetBenefits(string projectName, string employerID)
+    {
+      try
+      {
+        var handler = new BenefitsHandler();
+        var data = handler.GetBenefitsData(projectName, employerID);
+        return Ok(data);
+      }
+      catch (Exception error)
+      {
+        Console.WriteLine(error);
+        return BadRequest(error.Message);
+      }
+    }
+    */
+
+  }
+}
---
+++ ex2_B95447/ex2_backend/Handlers/CambioHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ex2_backend.Handlers
+{
+  public class CambioHandler
+  {
+
+    static List<string> cMoneda = new List<string>()
+    {
+      "20",
+      "30",
+      "50",
+      "25"
+    };
+
+    static List<string> nMoneda = new List<string>()
+    {
+      "500",
+      "100",
+      "50",
+      "25"
+    };
+
+  }
+}
